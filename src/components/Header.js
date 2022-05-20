dc084dd6de8094cbd9851087074ad1a02cec2e1f
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2022-05-20 09:53:36 -0300

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="header--container">
+            <img className="header--logo" src="../images/TrollFace.png"></img>
+            <h3 className="header--tittle">Meme Generator</h3>
+            <h4 className="header--detail">React Course - Project 3</h4>
+        </div>
+    )
+}
